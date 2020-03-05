ad905401aa7fdc2fd310e9cb8752ea3777275974
Jordan <jordanshehane@gmail.com>
2020-03-04 20:04:50 -0700

    added else logic to check which box is checked.

--- tech-stuff/src/React1/Profile.js
+++ tech-stuff/src/React1/Profile.js
@@ -1,20 +1,34 @@
 import React, {useState} from 'react';
 
 const Profile = (props) =>{
-
+//hook statement for monitoring state for usertype with initial state being both false
     const [userType, setUserType] = useState({
       renter: false,
       owner: false
     })
+    //a handle submit that asks which boxes are checked and chanes the values stored in userType accordingly
     const handleSubmit = (event) => {
-          event.preventDefault()
-          if(document.getElementById("owner").checked = true;){
+          event.preventDefault();
+          if(document.getElementById("owner").checked = true && document.getElementById("renter").checked = true){
             setUserType({
-              renter:false,
+              renter:true,
               owner:true
             })
           }
+            else if(document.getElementById("owner").checked = true && document.getElementById("renter").checked = false){
+            setUserType({
+              renter:false,
+              owner:true
+            }
+          }
+            else if(document.getElementById("owner").checked = false && document.getElementById("renter").checked = true){
+            setUserType({
+              renter:true,
+              owner:false
+            }
+          }
     }
+    //return statement passing information from props, may just do a axios statement since this is part of mvp for me.
     return(
       <div className = "profile_container">
 
