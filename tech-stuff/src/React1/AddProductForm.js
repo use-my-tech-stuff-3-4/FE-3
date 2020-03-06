203fdd565e9c7aad55d412cf50d2b90d3932df99
Jordan <jordanshehane@gmail.com>
2020-03-05 19:43:07 -0700

    minor styling to addproductform component

--- tech-stuff/src/React1/AddProductForm.js
+++ tech-stuff/src/React1/AddProductForm.js
@@ -2,11 +2,33 @@ import React, { useState, useContext } from 'react';
 import { axiosWithAuth } from '../React2/authentication/axiosWithAuth'
 import UploadImage from '../React2/components/UploadImage'
 import { UserContext } from '../React2/context/UserContext';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme =>({
+  product_form:{
+    width: "600px",
+    height: "auto",
+    margin: "auto",
+    padding: "20px",
+    border: "5px solid black",
+    display: "flex",
+    flexdirection: "row"
+
+
+  },
+  form_item:{
+      padding: "5px"
+
+  },
+  form_item_container:{
+    padding: "5px"
+  }
+}))
 
 const AddProductForm = (props) => {
 
   let {user, setUser} = useContext(UserContext)
-
+  const classes = useStyles();
   //hook statement setting up state for new product
   const [newProduct, setNewProduct] = useState({
     owner: localStorage.getItem('userid'),
@@ -50,64 +72,82 @@ const AddProductForm = (props) => {
   };
   // le form
   return (
-    <section>
+    <section className= {classes.product_form}>
       <form onSubmit={handleSubmit}>
-        <UploadImage />
-        <label htmlFor="title"> Title </label>
-        <input
-          type="text"
-          placeholder="Enter Title"
-          id="title"
-          name="title"
-          value={newProduct.title}
-          onChange={handleChange}
-        />
-        <label htmlFor="type"> Type </label>
-        <input
-          type="text"
-          placeholder="Enter Type"
-          id="type"
-          name="type"
-          value={newProduct.type}
-          onChange={handleChange}
-        />
-        <label htmlFor="brand"> Brand </label>
-        <input
-          type="text"
-          placeholder="Enter Brand"
-          id="brand"
-          name="brand"
-          value={newProduct.brand}
-          onChange={handleChange}
-        />
-        <label htmlFor="model"> Model </label>
-        <input
-          type="text"
-          placeholder="Enter Model"
-          id="model"
-          name="model"
-          value={newProduct.model}
-          onChange={handleChange}
-        />
+        <div className = "form_item_container">
+            <div className ={classes.form_item}>
+            <label htmlFor="title"> Title </label>
+            <input
+              type="text"
+              placeholder="Enter Title"
+              id="title"
+              name="title"
+              value={newProduct.title}
+              onChange={handleChange}
+            />
+            </div>
 
-        <label htmlFor="description"> Description </label>
-        <input
-          type="text"
-          placeholder="Enter Description"
-          id="description"
-          name="description"
-          value={newProduct.description}
-          onChange={handleChange}
-        />
-        <label htmlFor="price"> Price </label>
-        <input
-          type="text"
-          placeholder="Enter Price"
-          id="price"
-          name="price"
-          value={newProduct.price}
-          onChange={handleChange}
-        />
+        <div className ={classes.form_item}>
+          <label htmlFor="type"> Type </label>
+          <input
+            type="text"
+            placeholder="Enter Type"
+            id="type"
+            name="type"
+            value={newProduct.type}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div className = "form_item_container">
+        <div className ={classes.form_item}>
+          <label htmlFor="brand"> Brand </label>
+          <input
+            type="text"
+            placeholder="Enter Brand"
+            id="brand"
+            name="brand"
+            value={newProduct.brand}
+            onChange={handleChange}
+          />
+        </div>
+        <div className ={classes.form_item}>
+          <label htmlFor="model"> Model </label>
+          <input
+            type="text"
+            placeholder="Enter Model"
+            id="model"
+            name="model"
+            value={newProduct.model}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div className = "form_item_container">
+        <div className ={classes.form_item}>
+          <label htmlFor="description"> Description </label>
+          <input
+            type="text"
+            placeholder="Enter Description"
+            id="description"
+            name="description"
+            value={newProduct.description}
+            onChange={handleChange}
+          />
+        </div>
+        <div className ={classes.form_item}>
+          <label htmlFor="price"> Price </label>
+          <input
+            type="text"
+            placeholder="Enter Price"
+            id="price"
+            name="price"
+            value={newProduct.price}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <UploadImage className = {classes.image_select}/>
         <button type='submit'>Add Product</button>
       </form>
 
